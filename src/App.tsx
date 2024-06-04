97183367e982a4d8d21e45b4b570b7cb30dc294d
Bhadra-design <gbhadra919@gmail.com>
2024-06-04 14:02:51 +0530

    feat: Add new pages and routes for explore, saved, all users, create post, update post, post details, profile, update profile, and liked posts
    chore: Uncomment the route for liked posts in App.tsx
    
    chore: Update RootLayout to include top bar, left sidebar, outlet, and bottom bar
    
    chore: Add new page components for saved, explore, profile, all users, create post, update post, post details, update profile, and liked posts
    
    feat: Add signOutAccount function to handle user sign out
    
    chore: Update queriesAndMutations.ts to include useSignOutAccount hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from "react";
 import "./global.css";
 import { Routes, Route } from "react-router-dom";
 import SignInForm from "./_auth/forms/SignInForm";
 import SignUpForm from "./_auth/forms/SignUpForm";
-import { Home } from "./_root/pages";
+import {
+  Home,
+  Explore,
+  Saved,
+  AllUsers,
+  CreatePost,
+  UpdatePost,
+  PostDetails,
+  Profile,
+  UpdateProfile,
+  LikedPosts,
+} from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "@/components/ui/toaster";
@@ -21,6 +31,16 @@ const App = () => {
         {/* Private Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<UpdatePost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id/" element={<UpdateProfile />} />
+          {/* <Route path="/liked-posts" element={<LikedPosts />} /> */} //TODO: Uncomment this line
+
         </Route>
       </Routes>
       <Toaster />
--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,21 @@
+import BottomBar from '@/components/shared/BottomBar'
+import LeftSibar from '@/components/shared/LeftSibar'
+import TopBar from '@/components/shared/TopBar'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className='w-full md:flex'>
+      <TopBar />
+      <LeftSibar />
+
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+
+      <BottomBar />
+    </div>
   )
 }
 
